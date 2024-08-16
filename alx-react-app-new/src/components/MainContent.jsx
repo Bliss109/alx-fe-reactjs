5ece15cc7879ebd817b0d15cfcea92c8b00c1f0d
Bliss109 <captainb140@gmail.com>
2024-08-16 16:25:34 +0300

    Inline Styling in JSX

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function MainContent(){
+        return (
+         <main style= {{fontWeight: '600', fontFamily: 'sans-serif', textAlign: 'left'}}>
+  <p>I love to visit New York, Paris, and Tokyo.</p>
+</main>
+	);
+}
+export default MainContent;
